0970af6784499fa4cc08ffebc28df5ce33d6e9a8
tiasma92 <mattias.azoulay@gmail.com>
2020-03-11 18:19:12 +0100

    modif stack

--- Components/Navigation/Navigation.js
+++ Components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer'
 import {createStackNavigator} from 'react-navigation-stack';
 import Signin from '../Screens/Signin';
 import Profil from '../Screens/Profil';
@@ -21,14 +21,52 @@ import HomeHelper from '../Screens/HomeHelper';
 import HomeHelp from '../Screens/HomeHelp';
 import HelperValid from '../Screens/HelperValid';
 import Contact from "../Screens/Contact";
-import { Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
-import { Button } from 'react-native-elements';
-import Menu,{ MenuItem, MenuDivider } from 'react-native-material-menu';
 
-const AppNavigator = createStackNavigator(
+
+// class NavigationDrawerStructure extends Component {
+  //Structure for the navigatin Drawer
+  // toggleDrawer = () => {
+    //Props to open/close the drawer
+//     this.props.navigationProps.toggleDrawer();
+//   };
+//   render() {
+//     return (
+//       <View style={{ flexDirection: 'row' }}>
+//         <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
+//           {/*Donute Button Image */}
+//           <Image
+//             source={require('./image/drawer.png')}
+//             style={{ width: 25, height: 25, marginLeft: 5 }}
+//           />
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+// }
+
+const LoginStack = createStackNavigator({
+  accueil: {
+    screen: Accueil
+  },
+  signin: {
+    screen: Signin
+  },
+  signup: {
+    screen: Signup
+  }
+},
+  {
+    initialRouteName: "accueil",
+    defaultNavigationOptions: {
+       headerStyle: {
+        backgroundColor: "#2C5F13",
+       },
+       headerTintColor: '#fff',
+      }
+})
+
+const NavNavigator = createStackNavigator(
     {
-      signup: Signup,
-      signin: Signin,
       Home: Homepage,
       profil: Profil,
       forget: PasswordForget,
@@ -40,7 +78,6 @@ const AppNavigator = createStackNavigator(
       historyhelp: HistoryHelp,
       comment: Comments,
       myhelp: MyHelp,
-      accueil:Accueil,
       confirmD:DemandeConf,
       HomeH:HomeHelp,
       HomeA:HomeHelper,
@@ -49,7 +86,6 @@ const AppNavigator = createStackNavigator(
       
     },
     {
-      initialRouteName: 'accueil',
       defaultNavigationOptions: {
          headerStyle: {
           backgroundColor: "#2C5F13",
@@ -59,6 +95,44 @@ const AppNavigator = createStackNavigator(
          }
   })
 
+
+
+// const DrawerNavigator = createDrawerNavigator({
+//   profil: {
+//     //Title
+//     screen: Profil,
+//     navigationOptions: {
+//       drawerLabel: 'Profil',
+//     },
+//   },
+//   myhelp: {
+//     //Title
+//     screen: MyHelp,
+//     navigationOptions: {
+//       drawerLabel: 'Mes aides',
+//     },
+//   },
+//   historyhelp: {
+//     //Title
+//     screen: HistoryHelp,
+//     navigationOptions: {
+//       drawerLabel: "Mon historique d'aides",
+//     },
+//   },
+// });
+
+const AppNavigator = createAppContainer(
+  createSwitchNavigator(
+    {
+      Login: LoginStack,
+      Nav: NavNavigator,
+      // drawer: DrawerNavigator
+    },
+    {
+      initialRouteName: "Login"
+    }
+  )
+)
   
   
 export default Navigation = createAppContainer(AppNavigator)
--- Components/Screens/ip.js
+++ Components/Screens/ip.js
@@ -1,3 +1,3 @@
-var ipAdress = "10.2.4.23"
+var ipAdress = "192.168.1.96"
 
 module.exports = ipAdress;
